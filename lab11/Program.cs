428609e5d1e955d3dfea3d98776a3d666d91e2ea
Eugene Chevzhik <eugene.chevzh@gmail.com>
2018-10-12 15:51:07 +0300

    123

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -10,6 +10,43 @@ namespace lab11
     {
         static void Main(string[] args)
         {
+            string[] months = { "January", "Fabruary", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December" };
+
+            int stringCount = Convert.ToInt32(Console.ReadLine());
+
+            var result = from m in months
+                         where m.Count() == stringCount
+                         select m;
+
+            foreach(var res in result)
+            {
+                Console.WriteLine(res);
+            }
+
+
+            result = from m in months
+                     where m.Contains("u")
+                     where m.Length >= 4
+                     orderby m
+                     select m;
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Месяцы, содержащие букву u с длинной не менее 4-х символов, отсортированные в алфавитном порядке");
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+
+            
+            Console.WriteLine("\n");
+            Console.WriteLine("Только летние и зимние месяцы");
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+
         }
     }
 }
