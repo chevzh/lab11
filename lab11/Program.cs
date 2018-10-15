9178c67e0878cf51d9d657d57ce3343ca6ff6228
Eugene Chevzhik <eugene.chevzh@gmail.com>
2018-10-15 17:20:43 +0300

    almost done

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -39,16 +39,48 @@ namespace lab11
                 Console.WriteLine(res);
             }
 
+            result = months.Select(m => m).Where(m => m.Contains("u") || m == "December");
+            
+            Console.WriteLine("\n");
+            Console.WriteLine("Только летние и зимние месяцы");
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
 
-                               
+            List<MyDate> list = new List<MyDate>() {new MyDate(06,04,1996), new MyDate(1,2,1023), new MyDate(10,12,2018),
+                new MyDate(5,4,1023),new MyDate(1,6,1025), new MyDate(01,1,1996) };
 
-            //Console.WriteLine("\n");
-            //Console.WriteLine("Только летние и зимние месяцы");
-            //foreach (var res in result)
-            //{
-            //    Console.WriteLine(res);
-            //}
+            var years = list.Select(d => d).Where(d => d.Year == 1996);
+            var month = list.Select(d => d).Where(d => d.Month == 4);
+            var poolDate = list.Select(d => d).Where(d => d.Year <= 1996 || d.Year >= 1023);
+            var biggestDate = list.Max();
+            var firstDate = list.Select(d => d).Where(d => d.Day == 1).First();
+            list.Sort();
 
+            
+
+            Dictionary<string, IEnumerable<MyDate>> listToPrint = new Dictionary<string, IEnumerable<MyDate>>()
+            {
+                { "-----------Даты 1996 года-----------" , list.Select(d => d).Where(d => d.Year == 1996)},
+                
+                
+            };
+
+            Print(listToPrint);
+
+            void Print(Dictionary<string, IEnumerable<MyDate>> dictionary)
+            {
+                foreach (var el in dictionary)
+                {
+                    Console.WriteLine(el.Key);
+                    foreach(var v in el.Value)
+                    {
+                        v.PrintDate();
+                    }
+                }
+            }
+           
         }
     }
 }
